b78cd08ce0e6a9bd2b1c30af3ffc70369ebdaec9
Göncző Kristóf <kristofgonczo@gmail.com>
2021-07-17 12:23:24 +0200

    Creating project

---
+++ admin/src/test/java/dev/coralwombat/appointment/booking/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.coralwombat.appointment.booking.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ entities/src/main/java/dev/coralwombat/appointment/booking/Main.java
@@ -0,0 +1,8 @@
+package dev.coralwombat.appointment.booking;
+
+public class Main {
+
+	public static void main(String[] args) {
+	}
+
+}
