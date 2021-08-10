ea8fd211bea1d5782aceae92f606100852a39c67
Göncző Kristóf <kristofgonczo@gmail.com>
2021-08-10 16:38:42 +0200

    fix building problem

--- client-service/src/test/java/dev/coralwombat/appointment/booking/clientservice/ClientServiceApplicationTests.java
+++ client-service/src/test/java/dev/coralwombat/appointment/booking/clientservice/ClientServiceApplicationTests.java
@@ -3,7 +3,7 @@ package dev.coralwombat.appointment.booking.clientservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes=JPAConfiguration.class)
 class ClientServiceApplicationTests {
 
 	@Test
---
+++ client-service/src/test/java/dev/coralwombat/appointment/booking/clientservice/JPAConfiguration.java
@@ -0,0 +1,50 @@
+package dev.coralwombat.appointment.booking.clientservice;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@TestConfiguration
+@EnableTransactionManagement
+public class JPAConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+	var dataSource = new DriverManagerDataSource();
+
+	dataSource.setUsername("testuser");
+	dataSource.setPassword("testpass");
+	dataSource.setUrl("jdbc:h2:file:./database");
+
+	return dataSource;
+    }
+
+    Properties additionalProperties() {
+	var properties = new Properties();
+	properties.setProperty("hibernate.hbm2ddl.auto", "create");
+	properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+
+	return properties;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+	var em = new LocalContainerEntityManagerFactoryBean();
+	em.setDataSource(dataSource());
+	em.setPackagesToScan("dev.coralwombat.appointment.booking.entities");
+
+	JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+	em.setJpaVendorAdapter(vendorAdapter);
+	em.setJpaProperties(additionalProperties());
+
+	return em;
+    }
+
+}
