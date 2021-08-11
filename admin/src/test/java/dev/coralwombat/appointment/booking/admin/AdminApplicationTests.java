eb8d464bdad32fd373984f27c5a424e84c0e23e0
Göncző Kristóf <kristofgonczo@gmail.com>
2021-08-11 19:44:45 +0200

    Adding assertations to contextLoad tests

--- admin/src/test/java/dev/coralwombat/appointment/booking/admin/AdminApplicationTests.java
+++ admin/src/test/java/dev/coralwombat/appointment/booking/admin/AdminApplicationTests.java
@@ -3,11 +3,14 @@ package dev.coralwombat.appointment.booking.admin;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class AdminApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
 
 }
--- client-service/src/test/java/dev/coralwombat/appointment/booking/clientservice/ClientServiceApplicationTests.java
+++ client-service/src/test/java/dev/coralwombat/appointment/booking/clientservice/ClientServiceApplicationTests.java
@@ -3,11 +3,14 @@ package dev.coralwombat.appointment.booking.clientservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes=JPAConfiguration.class)
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = JPAConfiguration.class)
 class ClientServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
 
 }
